7d1a5a09735a990ab2e659102ce365ad7b53eea2
Alioukara <ali.oukara@epitech.eu>
2022-01-30 00:21:35 +0100

    algo ok css ok

--- barrelGame.js
+++ barrelGame.js
@@ -7,15 +7,24 @@ const titlebaril8 = document.getElementById("valeur8")
 const titlebaril5 = document.getElementById("valeur5")
 const titlebaril3 = document.getElementById("valeur3")
 
+const barB8 = document.getElementById("barB8")
+const barB5 = document.getElementById("barB5")
+const barB3 = document.getElementById("barB3")
+
 const baril8Data = { value: 8, capacity: 8, name: "baril8" }
 const baril5Data = { value: 0, capacity: 5, name: "baril5" }
 const baril3Data = { value: 0, capacity: 3, name: "baril3" }
+
+
 
 baril8.addEventListener('click', setStorage)
 baril5.addEventListener('click', setStorage)
 baril3.addEventListener('click', setStorage)
 
-
+document.getElementById("restart").addEventListener('click', () =>{
+    localStorage.clear()
+    window.location.reload();
+})
 
 function jeugagne() {
 
@@ -25,8 +34,9 @@ function jeugagne() {
     
  }
 }
-
-
+ function updateProgressBar(capacity, value) {
+    document.getElementById("barB" + capacity).setAttribute("value", value);
+ }
 
 function setStorage(e) {
  
@@ -35,18 +45,21 @@ function setStorage(e) {
             checkandSet(baril8Data)
 
             calculate(baril8Data)
+            
             jeugagne()
             break;
         case "baril5":
             checkandSet(baril5Data)
 
             calculate(baril5Data)
+            
             jeugagne()
             break;
         case "baril3":
             checkandSet(baril3Data)
 
             calculate(baril3Data)
+           
             jeugagne()
             break;
     }
@@ -54,21 +67,32 @@ function setStorage(e) {
 
 }
 
+function deleteScale() {
+    document.getElementById(localStorage.getItem("clicked1")).style.removeProperty("transform");
+}
+
 function checkandSet(barilobj) {
+    // if (localStorage.getItem("clicked2")) {
+    //     document.getElementById(localStorage.getItem("clicked1")).style.removeProperty("transform");
+
+    // }
     if (baril8Data.value == 4 && baril5Data.value == 4) {
         alert("bravo")
     }
     if (!localStorage.getItem("clicked2") && localStorage.getItem("clicked1") === barilobj.name) {
+        deleteScale()
         localStorage.clear()
         return
     }
     if (!localStorage.getItem("clicked1") && !localStorage.getItem("clicked2")) {
         localStorage.setItem("clicked1", barilobj.name)
+         document.getElementById(localStorage.getItem("clicked1")).style.cssText +=  "transform: scale(1.1)";
         localStorage.setItem("valueofclicked1", barilobj.value)
         localStorage.setItem("capacityofclicked1", barilobj.capacity)
         return
     }
     localStorage.getItem("clicked1") ? localStorage.setItem("clicked2", barilobj.name) : localStorage.setItem("clicked1", barilobj.name)
+    document.getElementById(localStorage.getItem("clicked1")).style.cssText +=  "transform: scale(1.1)";
     localStorage.getItem("valueofclicked1") ? localStorage.setItem("valueofclicked2", barilobj.value) : localStorage.setItem("valueofclicked1", barilobj.value)
     localStorage.getItem("capacityofclicked1") ? localStorage.setItem("capacityofclicked2", barilobj.capacity) : localStorage.setItem("capacityofclicked1", barilobj.capacity)
 }
@@ -77,7 +101,7 @@ function calculate(objbaril) {
         alert("bravo")
     }
     if (localStorage.getItem("clicked1") && localStorage.getItem("clicked2")) {
-
+        document.getElementById(localStorage.getItem("clicked1")).style.removeProperty("transform");
         var newvalue = parseInt(localStorage.getItem("valueofclicked2")) + parseInt(localStorage.getItem("valueofclicked1"))
 
         objbaril.value = newvalue
@@ -138,7 +162,9 @@ function calculate(objbaril) {
         }
 
         console.log("baril8", baril8Data.value, "baril5", baril5Data.value, "baril3", baril3Data.value)
-
+        updateProgressBar(baril8Data.capacity, baril8Data.value)
+        updateProgressBar(baril5Data.capacity, baril5Data.value)
+        updateProgressBar(baril3Data.capacity, baril3Data.value)
         localStorage.clear()
     }
 
